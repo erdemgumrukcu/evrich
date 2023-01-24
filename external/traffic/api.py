4f615c0e8153921943343d512eb418091f9f9a2f
Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>
2023-01-24 11:01:05 +0100

    Place-holder endpoints for demonstration of routing service.
    Signed-off-by: Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>

---
+++ external/traffic/api.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 20 17:50:39 2022
+
+@author: egu
+"""
+
+from typing import Union
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+class TFRequest(BaseModel):
+       
+    vehicle_model: str
+    battery_energy_capacity: float
+    drive_start_location: Union[str,None]=None
+    drive_start_SOC: float = 0.0
+    drive_start_time: Union[float,None]=None
+    candidate_hosts: list
+    
+    
+app = FastAPI()
+
+@app.post("/trafficforecast/")
+async def provide_forecast(item: TFRequest):
+    
+    response={}
+    
+    for aggregator in item.candidate_hosts:
+        
+        #TODO: Add more sophisticated estimation logic or another request post
+        #Currently each cluster is accessed through identical road conditions: no delay-no deviations
+        response[aggregator]={}
+        response[aggregator]['estimate_arrival_SOC']=item.drive_start_SOC
+        
+        if aggregator=='aggregator1':
+            response[aggregator]['estimate_arrival_time']=item.drive_start_time+300
+        else:
+            response[aggregator]['estimate_arrival_time']=item.drive_start_time
+         
+    print(item)
+    print(response)
+    return response
